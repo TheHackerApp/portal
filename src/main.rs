694a59b1264a84b2106ad85e4b9381c4916bdf98
Alex Krantz <alex@krantz.dev>
2024-05-11 14:38:33 -0700

    Add basic runtime framework

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
+use eyre::WrapErr;
+
+#[tokio::main]
+async fn main() -> eyre::Result<()> {
+    color_eyre::install()?;
+    dotenv()?;
+
     println!("Hello, world!");
+
+    Ok(())
+}
+
+/// Load environment variables from a .env file, if it exists.
+fn dotenv() -> eyre::Result<()> {
+    if let Err(error) = dotenvy::dotenv() {
+        if !error.not_found() {
+            return Err(error).wrap_err("failed to load .env");
+        }
+    }
+
+    Ok(())
 }
