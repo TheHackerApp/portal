d0cba15e9f5c4e6284a95cf1db895900d2bcb178
Alex Krantz <alex@krantz.dev>
2024-05-31 22:28:22 -0700

    Distinguish between null and undefined for saving applications

--- graphql/src/mutation/draft_application.rs
+++ graphql/src/mutation/draft_application.rs
@@ -1,6 +1,6 @@
 use super::{results, UserError};
 use crate::errors::Forbidden;
-use async_graphql::{Context, InputObject, Object, Result, ResultExt};
+use async_graphql::{Context, InputObject, MaybeUndefined, Object, Result, ResultExt};
 use chrono::NaiveDate;
 use context::{checks, UserRole};
 use database::{Application, DraftApplication, Education, Gender, PgPool, RaceEthnicity};
@@ -11,6 +11,16 @@ results! {
         /// The saved, but unsubmitted application
         draft_application: DraftApplication,
     }
+}
+
+macro_rules! set_option {
+    ($source:expr => $destination:expr) => {
+        match $source {
+            MaybeUndefined::Value(value) => $destination = Some(value),
+            MaybeUndefined::Null => $destination = None,
+            MaybeUndefined::Undefined => {}
+        }
+    };
 }
 
 #[derive(Default)]
@@ -49,22 +59,26 @@ impl Mutation {
             .extend()?
             .unwrap_or_else(|| DraftApplication::new(scope.event.clone(), user.id));
 
-        draft.gender = input.gender;
-        draft.race_ethnicity = input.race_ethnicity;
-        draft.date_of_birth = input.date_of_birth;
-        draft.education = input.education;
-        draft.graduation_year = input.graduation_year;
-        draft.major = input.major;
-        draft.hackathons_attended = input.hackathons_attended;
-        draft.links = input.links;
-        draft.address_line1 = input.address_line1;
-        draft.address_line2 = input.address_line2;
-        draft.address_line3 = input.address_line3;
-        draft.locality = input.locality;
-        draft.administrative_area = input.administrative_area;
-        draft.postal_code = input.postal_code;
-        draft.country = input.country;
-        draft.share_information = input.share_information;
+        set_option!(input.gender => draft.gender);
+        set_option!(input.race_ethnicity => draft.race_ethnicity);
+        set_option!(input.date_of_birth => draft.date_of_birth);
+        set_option!(input.education => draft.education);
+        set_option!(input.graduation_year => draft.graduation_year);
+        set_option!(input.major => draft.major);
+        set_option!(input.hackathons_attended => draft.hackathons_attended);
+        if let Some(links) = input.links {
+            draft.links = links;
+        }
+        set_option!(input.address_line1 => draft.address_line1);
+        set_option!(input.address_line2 => draft.address_line2);
+        set_option!(input.address_line3 => draft.address_line3);
+        set_option!(input.locality => draft.locality);
+        set_option!(input.administrative_area => draft.administrative_area);
+        set_option!(input.postal_code => draft.postal_code);
+        set_option!(input.country => draft.country);
+        if let Some(share_information) = input.share_information {
+            draft.share_information = share_information;
+        }
 
         draft.save(&mut txn).await.extend()?;
 
@@ -78,39 +92,39 @@ impl Mutation {
 #[derive(Debug, InputObject)]
 struct SaveApplicationInput {
     /// The participant's gender
-    pub gender: Option<Gender>,
+    pub gender: MaybeUndefined<Gender>,
     /// The participant's race/ethnicity
-    pub race_ethnicity: Option<RaceEthnicity>,
+    pub race_ethnicity: MaybeUndefined<RaceEthnicity>,
     /// Participant birthday
-    pub date_of_birth: Option<NaiveDate>,
+    pub date_of_birth: MaybeUndefined<NaiveDate>,
 
     /// The highest level of education the participant has achieved/is working on
-    pub education: Option<Education>,
+    pub education: MaybeUndefined<Education>,
     /// When the participant will graduate/graduated
-    pub graduation_year: Option<i32>,
+    pub graduation_year: MaybeUndefined<i32>,
     /// What the participant is studying
-    pub major: Option<String>,
+    pub major: MaybeUndefined<String>,
 
     /// How many hackathons the participant has attended
-    pub hackathons_attended: Option<i32>,
+    pub hackathons_attended: MaybeUndefined<i32>,
     /// Public links the participant wishes to share (i.e. portfolio, GitHub, etc.)
-    pub links: Vec<String>,
+    pub links: Option<Vec<String>>,
 
     /// The first line of the shipping address
-    pub address_line1: Option<String>,
+    pub address_line1: MaybeUndefined<String>,
     /// The second line of the shipping address
-    pub address_line2: Option<String>,
+    pub address_line2: MaybeUndefined<String>,
     /// The last line of the shipping address
-    pub address_line3: Option<String>,
+    pub address_line3: MaybeUndefined<String>,
     /// The city/town of the shipping address
-    pub locality: Option<String>,
+    pub locality: MaybeUndefined<String>,
     /// The state/province/region of the shipping address
-    pub administrative_area: Option<String>,
+    pub administrative_area: MaybeUndefined<String>,
     /// The postal code of the shipping address
-    pub postal_code: Option<String>,
+    pub postal_code: MaybeUndefined<String>,
     /// The ISO code of the country the shipping address is located in
-    pub country: Option<String>,
+    pub country: MaybeUndefined<String>,
 
     /// Whether the participant wishes to share information with sponsors
-    pub share_information: bool,
+    pub share_information: Option<bool>,
 }
